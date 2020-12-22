19ec4695a736b15f4f1c2bb338104d6b438fed84
arunabhamidipati <arunprasad_bh@yahoo.com>
2020-12-22 21:50:52 +0000

    Added new module converters

--- web-app/src/main/java/guru/springframework/controllers/UserController.java
+++ web-app/src/main/java/guru/springframework/controllers/UserController.java
@@ -1,10 +1,11 @@
 package guru.springframework.controllers;
 
+import guru.springframework.converters.UserMapper;
 import guru.springframework.domain.UserCommand;
 import guru.springframework.entities.User;
 
 public class UserController {
     User SaveUser(UserCommand userCommand ){
-        return new User();
+        return  UserMapper.INSTANCE.userCommandToUser(userCommand);
     }
 }
